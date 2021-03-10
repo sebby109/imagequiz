e5f8156d4173cfb056cb480f064905258efe2e83
sebby109 <sebastianga14@hotmail.com>
2021-03-09 18:34:37 -0700

    first compents set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Container fluid>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" ></Route>
+        </Switch>
+      </Container>
+    </HashRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+
+function Home(){
+    return(
+        <div>hello</div>
+    );
+
+}
+
+export default Home;
