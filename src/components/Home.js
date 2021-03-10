bb804b33f30b266a83f744ecfe4812a25af7a9c7
sebby109 <sebastianga14@hotmail.com>
2021-03-10 16:22:21 -0700

    testing changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,34 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
+import flowers from './data';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reactDom from 'react-dom';
 
 
-function Home(){
-    return(
-        <div>hello</div>
+function Home() {
+
+    let getImg = () => {
+        const getDiv = document.getElementById("first");
+        const listItems = flowers.map((flowers) =>
+            <li>{ <img src={flowers['picture']} /> } <br/> {flowers['name']} </li>
+        );
+        ReactDOM.render(
+            <ul>{listItems}</ul>,
+            getDiv
+        );
+    }
+
+    return (
+        <Row>
+            <Col>
+
+                <div id="first" callFunc={getImg()}></div>
+            </Col>
+        </Row>
     );
+
 
 }
 
