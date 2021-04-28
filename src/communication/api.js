d64081cb13befafa77ea46d6e12a80ad43a0f307
sebby109 <sebastianga14@hotmail.com>
2021-04-28 11:49:54 -0700

    running deploy

--- src/communication/api.js
+++ src/communication/api.js
@@ -1,23 +1,12 @@
-let apiHost = "https://sebby109-imagequiz.herokuapp.com/";
+let apiHost = "https://sebby109-imagequiz.herokuapp.com";
 
 let getPlaces = () => {
-    return fetch(apiHost + '/places')
+    return fetch(apiHost + "/places")
     .then(response => response.json());
 }
 
-let addPlace = (place) => {
-        return fetch(apiHost + '/place', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(place)
-        })
-    }
-
 let api = {
     getPlaces: getPlaces
-    //addPlace: addPlace
 };
-// continue HOW TO CONNECT TO REST API...@15:33
+
 export default api;
